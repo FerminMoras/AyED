6fa6178b05db10cc53e7f7e5cdba4cfdc74b30d4
Fermin Moras <feer.moras@gmail.com>
2025-04-09 11:10:50 -0300

    Add files via upload

--- AyED/src/Practica2/ParcialArboles.java
+++ AyED/src/Practica2/ParcialArboles.java
@@ -65,6 +65,38 @@ public class ParcialArboles {
 		else {
 			return false;
 		}
+			
+	}
+	
+	//EJERCICIO 8
+	
+	private boolean calcularPrefijo(BinaryTree<Integer> arbol1, BinaryTree<Integer> arbol2) {
+		
+		if (arbol1.getData() != arbol2.getData()) {
+			return false;
+		}
+		
+		boolean hI = true;
+		boolean hD = true;
+		
+		if((arbol1.hasLeftChild() && arbol2.hasLeftChild())) {
+			hI = calcularPrefijo(arbol1.getLeftChild(), arbol2.getLeftChild());
+		}
+		
+		if((arbol1.hasRightChild() && arbol2.hasRightChild())) {
+			hD = calcularPrefijo(arbol1.getRightChild(), arbol2.getRightChild());
+		}
+		
+		return hI && hD;
+	}
+	
+	public boolean esPrefijo(BinaryTree<Integer> arbol1, BinaryTree<Integer> arbol2) {
+		
+		if (arbol1.isEmpty() || arbol2.isEmpty()) {
+			return false;
+		}
+	
+		return calcularPrefijo(arbol1,arbol2);
 	}
 	
 	public static void main(String[] args) {
@@ -98,5 +130,21 @@ public class ParcialArboles {
 		System.out.println("Resultado con valor 19: " + p.isLeftTree(19));
 		System.out.println("Resultado con valor -3: " + p.isLeftTree(-3));
 		System.out.println("Resultado con valor 22: " + p.isLeftTree(22));
+		
+		BinaryTree<Integer> arbol2 = new BinaryTree<Integer>(2);
+		BinaryTree<Integer> nodo7 = new BinaryTree<Integer>(7);
+		BinaryTree<Integer> nodo5 = new BinaryTree<Integer>(-5);
+		
+		arbol2.addLeftChild(nodo7);
+		arbol2.addRightChild(nodo5);
+		
+		nodo7.addRightChild(new BinaryTree<Integer>(6));
+		nodo5.addLeftChild(new BinaryTree<Integer>(19));
+		
+		if (p.esPrefijo(arbol, arbol2)) {
+			System.out.println ("Los arboles son prefijos");
+		}
+		else
+			System.out.println ("Los arboles no coinciden");
 	}
 }
